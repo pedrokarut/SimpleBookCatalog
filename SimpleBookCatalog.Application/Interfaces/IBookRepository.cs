9133b16ab5b5f5146c46fba9246eb55678c8b48b
Pedro Karut Gimenez <pedrokarut@hotmail.com>
2025-03-20 10:23:13 -0300

    ajustes

--- SimpleBookCatalog.Application/Interfaces/IBookRepository.cs
+++ SimpleBookCatalog.Application/Interfaces/IBookRepository.cs
@@ -12,5 +12,12 @@ namespace SimpleBookCatalog.Application.Interfaces
         Task AddASync(Book book);
 
         Task<List<Book>> GetAllAsync();
+
+        Task<Book> GetByIdAsync(int id);
+
+        Task UpdateAsync(Book book);
+
+        Task DeleteByIdAsync(int id);
+
     }
 }
--- SimpleBookCatalog.Infraestructure/Repositories/BookRepository.cs
+++ SimpleBookCatalog.Infraestructure/Repositories/BookRepository.cs
@@ -19,10 +19,32 @@ namespace SimpleBookCatalog.Infraestructure.Repositories
             await context.SaveChangesAsync();
         }
 
+        public async Task DeleteByIdAsync(int id)
+        {
+            var book = await GetByIdAsync(id);
+            if (book != null)
+            {
+                context.Books.Remove(book);
+                await context.SaveChangesAsync();
+            }
+        }
+
         public async Task<List<Book>> GetAllAsync()
         {
             var books = await context.Books.ToListAsync();
             return books;
+        }
+
+        public async Task<Book> GetByIdAsync(int id)
+        {
+            var book = await context.Books.FirstOrDefaultAsync(e => e.Id == id);
+            return book;
+        }
+
+        public async Task UpdateAsync(Book book)
+        {
+            context.Entry(book).State = EntityState.Modified;
+            await context.SaveChangesAsync();
         }
     }
 }
